7f6a8c172994879ce46f57b96f2ab275b7e3b277
Maxwell Babey <babeymax@gmail.com>
2022-05-02 22:35:53 -0700

    Move files to allow proper routing via server. Rename createaccnt.html and createaccnt.css to createAccount.html and createAccount.css.

--- scripts/client.js
+++ scripts/client.js
@@ -1,22 +1,20 @@
 
+// const express = require('express');
 
+// var client = new net.Socket();
+// client.connect(1337, '127.0.0.1', function() {
+// 	console.log('Connected');
+// 	client.write('Hello, server! Love, Client.');
+// });
 
+// client.on('data', function(data) {
+// 	console.log('Received: ' + data);
+// 	client.destroy(); // kill client after server's response
+// });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// client.on('close', function() {
+// 	console.log('Connection closed');
+// });
 
 
 
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const { Router } = require('express');
 const express = require('express');
 const session = require('express-session');
 //const http = require('http');
@@ -5,22 +6,34 @@ const fs = require('fs');
 const app = express();
 const mysql = require('mysql2');
 
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/views'));
+
+app.use(session({secret: 'shoredoes', name: 'groopsess', resave: false, saveUninitialized: true}));
+
 const port = 8000;
 app.listen(port, () => {
     console.log('Gro-Operate running on port: ' + port);
 });
 
 app.get('/', (req, res) => {
-    res.send("Hello wordl");
-
-    // if session, login
-    
-    // else, redirect to login.html
-    
+    if (req.session.loggedIn) {
+        res.redirect('/profile'); // /profile for now, but will be /home in later versions 
+    } else {
+        res.redirect('/login');
+    }
 });
 
+app.post('/createAccount', (req, res) =>{
+    console.log('Login Post');
+});
 
-app.get('/login', () => {
+app.get('/login', (req, res) => {
+    let loginPage = fs.readFileSync('./views/login.html', 'utf8');
+    res.set('server', 'serversomething');
+    res.set('x-powered-by', 'anything');
+    res.send(loginPage);
+    
     const con = mysql.createConnection({
         host: 'localhost',
         user: 'root',
@@ -30,10 +43,17 @@ app.get('/login', () => {
 
     con.connect(function(err) {
             if(err) throw err;
-            console.log("Poggers");
+            console.log("SQL Poggers");
             con.query("SELECT * FROM `user`", function (err, result, fields){
                     if(err) throw err;
                     console.log(result);
             });
     });
+});
+
+app.get('/createAccount', (req, res) => {
+    let createAccountPage = fs.readFileSync('./views/createAccount.html');
+    res.set('server', 'serversomething');
+    res.set('x-powered-by', 'anything');
+    res.send(createAccountPage);
 });

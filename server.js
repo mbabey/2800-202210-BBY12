83aa367368b67e7b0347508597d1c9c134653ac3
Maxwell Babey <babeymax@gmail.com>
2022-05-02 22:49:21 -0700

    Change name of createAccount(s) to create-account(s). Add redirection to create-account.html when clicking 'create account' button.

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
-const { Router } = require('express');
+// const { Router } = require('express');
 const express = require('express');
 const session = require('express-session');
-//const http = require('http');
 const fs = require('fs');
 const app = express();
 const mysql = require('mysql2');
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/views'));
 
-app.use(session({secret: 'shoredoes', name: 'groopsess', resave: false, saveUninitialized: true}));
+app.use(session({ secret: 'shoredoes', name: 'groopsess', resave: false, saveUninitialized: true }));
 
 const port = 8000;
 app.listen(port, () => {
@@ -24,36 +23,31 @@ app.get('/', (req, res) => {
     }
 });
 
-app.post('/createAccount', (req, res) =>{
-    console.log('Login Post');
-});
+app.route('/login')
+    .get((req, res) => {
+        let loginPage = fs.readFileSync('./views/login.html', 'utf8');
+        res.send(loginPage);
 
-app.get('/login', (req, res) => {
-    let loginPage = fs.readFileSync('./views/login.html', 'utf8');
-    res.set('server', 'serversomething');
-    res.set('x-powered-by', 'anything');
-    res.send(loginPage);
-    
-    const con = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'store'
-    });
+        const con = mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: '',
+            database: 'store'
+        });
 
-    con.connect(function(err) {
-            if(err) throw err;
+        con.connect(function (err) {
+            if (err) throw err;
             console.log("SQL Poggers");
-            con.query("SELECT * FROM `user`", function (err, result, fields){
-                    if(err) throw err;
-                    console.log(result);
+            con.query("SELECT * FROM `user`", function (err, result, fields) {
+                if (err) throw err;
+                console.log(result);
             });
+        });
+    }).post((req, res) => {
+        console.log('Login Post');
     });
-});
 
-app.get('/createAccount', (req, res) => {
-    let createAccountPage = fs.readFileSync('./views/createAccount.html');
-    res.set('server', 'serversomething');
-    res.set('x-powered-by', 'anything');
+app.get('/create-account', (req, res) => {
+    let createAccountPage = fs.readFileSync('./views/create-account.html', 'utf8');
     res.send(createAccountPage);
 });

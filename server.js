5e8f101574aeaaaedcfb7df4ab82d002f2f68216
Tissure <kyle.ng7512@gmail.com>
2022-05-03 14:04:28 -0700

    Add use strict, Add authentication not working

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+'use strict';
 // const { Router } = require('express');
 const express = require('express');
 const session = require('express-session');
@@ -29,7 +30,8 @@ app.listen(port, () => {
 });
 
 app.get('/', (req, res) => {
-    if (req.session.loggedIn) {
+    console.log(req.session.success);
+    if (req.session.user) {
         res.redirect('/profile'); // /profile for now, but will be /home in later versions 
     } else {
         res.redirect('/login');
@@ -37,27 +39,54 @@ app.get('/', (req, res) => {
 });
 
 app.route('/login')
-    .get((req, res) => {
+    .get((req, res, next) => {
         let loginPage = fs.readFileSync('./views/login.html', 'utf8');
         res.send(loginPage);
-
+        next();
     })
-    .post((req, res) => {
-        let username = req.body.username.trim();
-        console.log(username);
+    .post((req, res, next) => {
+        let email = req.body.email.trim();
+        console.log(email);
         console.log(req.body.password);
 
-        con.query('Select * from `user` Where `username` = ?', [username], function(err, results, fields) {
+        con.query('Select * from (`user`) Where (`username` = ?) AND (`password` = ?)', [email, req.body.password], function(err, results, fields) { // Change `username` to `email` in legit database
             if (results.length > 0) { //TODO: Change condition to password check;
                 console.log(results[0]);
+                //req.session.loggedIn = true;
+                req.session.regenerate(function() {
+                    // Store the user's primary key
+                    // in the session store to be retrieved,
+                    // or in this case the entire user object
+                    req.session.user = email;
+                    req.session.success = 'Authenticated as ' + email +
+                        ' click to <a href="/logout">logout</a>. ' +
+                        ' You may now access <a href="/restricted">/restricted</a>.';
+                    // res.redirect('/');
+                });
             } else {
-                console.log("Username password combination not found");
+                console.log("Email/password combination not found");
             }
         });
         res.redirect('/');
+        next();
     });
 
-app.get('/create-account', (req, res) => {
+app.get('/profile', (req, res) => {
+    console.log("At /profile");
+    console.log(req.session);
+});
+
+app.get('/create-account', (req, res, next) => {
     let createAccountPage = fs.readFileSync('./views/create-account.html', 'utf8');
     res.send(createAccountPage);
+    next();
+});
+
+app.get('/logout', (req, res) => {
+    req.session.destroy(function() {
+        res.redirect('/');
+    });
+
+    // req.session.loggedIn = false;
+    // res.redirect('/');
 });

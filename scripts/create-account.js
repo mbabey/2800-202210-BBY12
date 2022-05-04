19ab83398032d255d2e4c12f6e712fcf08b78800
Kyle <kyle.ng7512@gmail.com>
2022-05-04 12:12:33 -0700

    Add parsing of location

--- scripts/create-account.js
+++ scripts/create-account.js
@@ -1,66 +1,5 @@
-const express = require('express');
-const app = express();
-const fs = require("fs");
 const mysql = require('mysql2');
 const crypto = require('crypto');
-
-// Notice that this is a 'POST'
-app.post('/add-users', function(req, res) {
-    res.setHeader('Content-Type', 'application/json');
-
-    console.log("Name", req.body.name);
-    console.log("Email", req.body.email);
-
-    let connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'comp2800'
-    });
-    connection.connect();
-    // TO PREVENT SQL INJECTION, DO THIS:
-    // (FROM https://www.npmjs.com/package/mysql#escaping-query-values)
-    connection.query('INSERT INTO users (name, email) values (?, ?)', [req.body.name, req.body.email],
-        function(error, results, fields) {
-            if (error) {
-                console.log(error);
-            }
-            //console.log('Rows returned are: ', results);
-            res.send({ status: "success", msg: "Record added." });
-
-        });
-    connection.end();
-
-});
-
-
-
-// Put that updates values to change data stored in db
-app.put('/update-users', function(req, res) {
-    res.setHeader('Content-Type', 'application/json');
-
-    let connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'comp2800'
-    });
-    connection.connect();
-    console.log("update values", req.body.user_name, req.body.first_name, req.body.last_name,
-        req.body.email_address, req.body.password, req.body.id);
-    connection.query('UPDATE bby12users SET user_name = ? AND first_name = ? AND last_name = ? AND email_address = ? AND password = ? WHERE ID = ?', [req.body.user_name, req.body.first_name, req.body.last_name, req.body.email_address, req.body.password, req.body.id],
-        function(error, results, fields) {
-            if (error) {
-                console.log(error);
-            }
-            //console.log('Rows returned are: ', results);
-            res.send({ status: "Success", msg: "User information updated." });
-
-        });
-    connection.end();
-
-});
-
 
 module.exports = {
     createAccount: function(req, res) {
@@ -75,8 +14,11 @@ module.exports = {
         let username = req.body.username;
         let pass = req.body.password;
         let success = false;
-        if (username === req.body["username-verify"] && pass === req.body["password-verify"]) {
-            console.log("same");
+        if (checkUsername(username, req) && checkPassword(pass, req)) {
+            console.log(req.body);
+            let location = req.body["location-street"] + ", " + req.body["location-city"] + ", " + req.body["location-country"];
+            console.log(location);
+            //console.log([username, pass, req.body["first-name"], req.body["last-name"], req.body["company-name"], req.body["email"], req.body["phone-num"], location, req.body["description"]]);
             success = true;
         } else console.log("not same");
         // const hash = crypto.createHash('sha256').update(pass).digest('hex');
@@ -94,3 +36,11 @@ module.exports = {
         return success;
     }
 };
+
+function checkUsername(username, req) {
+    return (username && username === req.body["username-verify"]); // TODO: Add additional checks: ie. min length
+}
+
+function checkPassword(pass, req) {
+    return (pass && pass === req.body["password-verify"]); // TODO: Add additional checks: ie. min length
+}

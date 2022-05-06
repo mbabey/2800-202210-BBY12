374257f1369041a901fa6927729d372d717fce69
Maxwell Babey <babeymax@gmail.com>
2022-05-05 17:24:30 -0700

    Add condition for null table results

--- scripts/account.js
+++ scripts/account.js
@@ -20,13 +20,13 @@ docLoaded(() => {
     getData();
 
     function popThaSpots(data) {
-        document.querySelector('.business-name-block').innerHTML = (data[0].cName != undefined) ? data[0].cName : '';
-        document.querySelector('.business-owner-fname-block').innerHTML = (data[0].fName != undefined) ? data[0].fName : '';
-        document.querySelector('.business-owner-lname-block').innerHTML = (data[0].lName != undefined) ? data[0].lName : '';
-        document.querySelector('.business-email-block').innerHTML = (data[0].email != undefined) ? data[0].email : '';
-        document.querySelector('.business-phone-block').innerHTML = (data[0].phoneNo != undefined) ? data[0].phoneNo : '';
-        document.querySelector('.business-location-block').innerHTML = (data[0].location != undefined) ? data[0].location : '';
-        document.querySelector('.business-description-block').innerHTML = (data[0].description != undefined) ? data[0].description : '';
+        document.querySelector('.business-name-block').innerHTML = (data[0].cName != undefined && data[0].cName != null) ? data[0].cName : '';
+        document.querySelector('.business-owner-fname-block').innerHTML = (data[0].fName != undefined && data[0].fName != null) ? data[0].fName : '';
+        document.querySelector('.business-owner-lname-block').innerHTML = (data[0].lName != undefined && data[0].lName != null) ? data[0].lName : '';
+        document.querySelector('.business-email-block').innerHTML = (data[0].email != undefined && data[0].email != null) ? data[0].email : '';
+        document.querySelector('.business-phone-block').innerHTML = (data[0].phoneNo != undefined && data[0].phoneNo != null) ? data[0].phoneNo : '';
+        document.querySelector('.business-location-block').innerHTML = (data[0].location != undefined && data[0].location != null) ? data[0].location : '';
+        document.querySelector('.business-description-block').innerHTML = (data[0].description != undefined && data[0].description != null) ? data[0].description : '';
     }
 });
 

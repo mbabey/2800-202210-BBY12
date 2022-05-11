131afea32823dcfb9689edc25a94579f7373d0c5
Maxwell Babey <babeymax@gmail.com>
2022-05-11 12:00:01 -0700

    Add start script

--- server.js
+++ server.js
@@ -28,7 +28,6 @@ app.use(session({
 }));
 
 // mysql://bvi0o6i4puwihszs:t6j3hhjg82p5yi6v@g84t6zfpijzwx08q.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/ooesezqo9t1r5sup
-
 const herokuConConfig = {
     host: 'g84t6zfpijzwx08q.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
     user: 'bvi0o6i4puwihszs',

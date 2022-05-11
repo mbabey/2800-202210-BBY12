3556a8da7c6a0a6f16dfaa6b89d696a8172d8cf4
Betty Nguyen <bettyanhn@gmail.com>
2022-05-11 14:41:51 -0700

    Change javascript code to pull admin data from being inside a get function in server.js to a separate js file

--- server.js
+++ server.js
@@ -213,6 +213,18 @@ app.get('/get-admins', function (req, res) {
     });
 });
 
+app.get('/get-admin', function (req, res) {
+    if (req.session.loggedIn && req.session.admin == true) {
+        let session_username = req.session.username;
+        let admins = 'SELECT * FROM BBY_12_users WHERE BBY_12_users.username = ?';
+        con.query(admins, [session_username], function (err, results) {
+            if (err) throw "Query to database failed.";
+            res.setHeader('content-type', 'application/json');
+            res.send({ status: "success", rows: results });
+        });
+    }
+});;
+
 app.get('/get-admin-table', function (req, res) {
     let admins = 'SELECT * FROM BBY_12_admins';
     con.query(admins, function (err, results) {
@@ -226,46 +238,29 @@ app.route('/admin-view-accounts')
     .get(function (req, res) {
 
         if (req.session.loggedIn && req.session.admin == true) {
-            let session_username = req.session.username;
-            let admin = 'SELECT * FROM BBY_12_users WHERE BBY_12_users.username = ?';
-            let username = "<h3>";
-            let first_name = "<p>";
-            let last_name = "<p>";
-            let business_name = "<p>";
-            con.query(admin, [session_username], function (err, results) {
-
+            let users = 'SELECT * FROM BBY_12_users';
+            con.query(users, function (err, results) {
                 if (err) throw err;
-                username += results[0].username + "</h3>";
-                first_name += results[0].fName + "</p>";
-                last_name += results[0].lName + "</p>";
-                business_name += results[0].cName + "</p>";
-                let users = 'SELECT * FROM BBY_12_users';
-                con.query(users, function (err, results) {
-                    if (err) throw err;
 
-                    let table = "<table><tr><th>Username</th><th class=\"admin-user-info\">First Name</th><th class=\"admin-user-info\">Last Name</th><th class=\"admin-user-info\">Business Name</th></tr>";
-                    for (let i = 0; i < results.length; i++) {
-                        table += "<tr><td>" + results[i].username + "</td><td class=\"admin-user-info\">" +
-                            results[i].fName + "</td><td class=\"admin-user-info\">" +
-                            results[i].lName + "</td><td class=\"admin-user-info\">" +
-                            results[i].cName + "</td></tr>";
-                    }
-                    table += "</table>";
+                let table = "<table><tr><th>Username</th><th class=\"admin-user-info\">First Name</th><th class=\"admin-user-info\">Last Name</th><th class=\"admin-user-info\">Business Name</th></tr>";
+                for (let i = 0; i < results.length; i++) {
+                    table += "<tr><td>" + results[i].username + "</td><td class=\"admin-user-info\">" +
+                        results[i].fName + "</td><td class=\"admin-user-info\">" +
+                        results[i].lName + "</td><td class=\"admin-user-info\">" +
+                        results[i].cName + "</td></tr>";
+                }
+                table += "</table>";
 
-                    let adminViewAcc = fs.readFileSync('./views/admin-view-accounts.html', 'utf8');
-                    let adminViewAccDOM = new JSDOM(adminViewAcc);
-                    adminViewAccDOM.window.document.getElementById("user-list").innerHTML = table;
-                    adminViewAccDOM.window.document.getElementById("u-name").innerHTML = username;
-                    adminViewAccDOM.window.document.getElementById("name").innerHTML = first_name + last_name;
-                    adminViewAccDOM.window.document.getElementById("b-name").innerHTML = business_name;
-                    let adminViewAccPage = adminViewAccDOM.serialize();
-                    res.send(adminViewAccPage);
-                });
-            });
-        } else {
-            res.redirect("/");
-        }
-    });
+                let adminViewAcc = fs.readFileSync('./views/admin-view-accounts.html', 'utf8');
+                let adminViewAccDOM = new JSDOM(adminViewAcc);
+                adminViewAccDOM.window.document.getElementById("user-list").innerHTML = table;
+                let adminViewAccPage = adminViewAccDOM.serialize();
+                res.send(adminViewAccPage);
+        });
+    } else {
+        res.redirect("/");
+    }
+});
 
 app.route("/create-post")
     .get((req, res) => {

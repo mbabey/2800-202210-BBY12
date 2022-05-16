70d67efd21c13c3ec0bd7297fa07e83f5527d71b
Maxwell Babey <babeymax@gmail.com>
2022-05-16 15:52:06 -0700

    Add enter pressing functionality for login form. Login flow now fully functional

--- scripts/login.js
+++ scripts/login.js
@@ -1,31 +1,36 @@
 docLoaded(() => {
-  ['click'].forEach(e => {
-    document.querySelector('#login-submit').addEventListener(e, () => {
-      let data = { username: document.querySelector('input[name=\'username\']').value, 
-      password: document.querySelector('input[name=\'password\']').value };
-      console.log(data);
-      sendData(data);
+  document.querySelector('#login-submit').addEventListener('click', () => {
+    let data = {
+      username: document.querySelector('input[name=\'username\']').value,
+      password: document.querySelector('input[name=\'password\']').value
+    };
+    sendData(data);
+  });
+
+  // Add listener to each input to activate button on enter press.
+  document.querySelectorAll('.login-input').forEach((input) => {
+    input.addEventListener('keyup', (e) => {
+      if (e.keyCode === 13) { // If key pressed is enter key
+        document.querySelector('#login-submit').click();
+      }
     });
   });
 });
 
 async function sendData(data) {
   try {
-      let response = await fetch('/login', {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify(data)
-      });
-      console.log(response);
-      response = await response.text();
-      console.log(response);
-      response = JSON.parse(response);
-      console.log(response);
-      if (response.status == 'success') {
-        window.location.replace('/');
-      } else {
-        document.querySelector('#error-message').innerHTML = 'Error! Username/password combination not found!';
-      }
+    let response = await fetch('/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data)
+    });
+    response = await response.text();
+    response = JSON.parse(response);
+    if (response.status == 'success') {
+      window.location.replace('/');
+    } else {
+      document.querySelector('#error-message').innerHTML = 'Error! Username/password combination not found!';
+    }
   } catch (err) {
     if (err) throw err;
   }

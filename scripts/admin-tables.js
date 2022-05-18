1b2502c63eec0e25de3178785f21be9303ec6fde
Maxwell Babey <babeymax@gmail.com>
2022-05-18 13:04:35 -0700

    Refactor code in admin-tables.js

--- scripts/admin-tables.js
+++ scripts/admin-tables.js
@@ -28,6 +28,23 @@ function makeCardsClickable() {
   });
 }
 
+async function sendData(data, path, callback) {
+  try {
+    let response = await fetch(path, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    });
+    response = await response.text();
+    response = JSON.parse(response);
+    callback(response);
+  } catch (err) {
+    if (err) throw err;
+  }
+}
+
 async function getUserData() {
   try {
     let userData = await fetch('/get-all-users', {
@@ -44,85 +61,10 @@ async function getUserData() {
   }
 }
 
-function popAdminData(adminData) {
-  // ADMIN TABLE CREATED HERE
-  let adminCard = "<div class='admin-card-group'>";
-  for (let i = 0; i < adminData.rows.length; i++) {
-    adminCard += (`
-    <div class='admin-card'>
-      <div class='admin-card-header'>
-        <div class='admin-card-avatar-background'>
-          <img class='admin-card-avatar' src="./avatars/${adminData.rows[i].profilePic}" alt='Profile Picture'>
-        </div>
-      </div>
-      <div class='admin-card-info'>
-        <h3 class='admin-card-username'>${adminData.rows[i].username}</h3>
-      </div>
-      <a class='close' id='delete-admin-account' href='#'>
-        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512" width="16" height="16">
-          <g>
-            <path fill="currentColor" d="M286.161,255.867L505.745,36.283c8.185-8.474,7.951-21.98-0.523-30.165c-8.267-7.985-21.375-7.985-29.642,0   L255.995,225.702L36.411,6.118c-8.475-8.185-21.98-7.95-30.165,0.524c-7.985,8.267-7.985,21.374,0,29.641L225.83,255.867   L6.246,475.451c-8.328,8.331-8.328,21.835,0,30.165l0,0c8.331,8.328,21.835,8.328,30.165,0l219.584-219.584l219.584,219.584   c8.331,8.328,21.835,8.328,30.165,0l0,0c8.328-8.331,8.328-21.835,0-30.165L286.161,255.867z"/>
-          </g>
-        <span class="tooltip-text">Remove admin privilege</span>
-      </a>
-    </div>
-    `);
-
-    // Placeholder image path for image
-    // ./avatars/Logo.png' alt='Profile Picture'></div>
-
-  }
-  adminCard += "</div>";
-  document.getElementById("admin-list").innerHTML = adminCard;
-}
-
 function popUserData(userData) {
   // USER CARD CREATED HERE
-  let userCard = "<div class='user-card-group'>";
-  for (let i = 0; i < userData.rows.length; i++) {
-    userCard += (`
-    <div class='user-card'>
-
-      <div class='user-card-header'>
-        <div class='user-card-avatar-background'>
-          <img class='user-card-avatar' src='./avatars/${userData.rows[i].profilePic}' alt='Profile Picture'>
-        </div>
-      </div>
-      <div class='user-card-info'>
-        <h3 class='user-card-username'>${userData.rows[i].username}</h3>
-        <span class='user-card-business-name'>${userData.rows[i].cName} | </span>
-        <span class='user-card-first-name'>${userData.rows[i].fName} </span>
-        <span class='user-card-last-name'>${userData.rows[i].lName}</span>
-      </div>
-      <a class='close' id='delete-user-account' href='#'>
-      <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512" width="16" height="16">
-        <g>
-          <path fill="currentColor" d="M286.161,255.867L505.745,36.283c8.185-8.474,7.951-21.98-0.523-30.165c-8.267-7.985-21.375-7.985-29.642,0   L255.995,225.702L36.411,6.118c-8.475-8.185-21.98-7.95-30.165,0.524c-7.985,8.267-7.985,21.374,0,29.641L225.83,255.867   L6.246,475.451c-8.328,8.331-8.328,21.835,0,30.165l0,0c8.331,8.328,21.835,8.328,30.165,0l219.584-219.584l219.584,219.584   c8.331,8.328,21.835,8.328,30.165,0l0,0c8.328-8.331,8.328-21.835,0-30.165L286.161,255.867z"/>
-        </g>
-        <span class="tooltip-text">Delete user</span>
-    </a>
-    </div>
-    `);
-  }
-  userCard += "</div>";
+  let userCard = makeUserCard(userData);
   document.getElementById("user-list").innerHTML = userCard;
-}
-
-
-async function getAdminData() {
-  try {
-    let adminData = await fetch('/get-all-admins', {
-      method: 'GET'
-    });
-    adminData = await adminData.text();
-    adminData = JSON.parse(adminData);
-    if (adminData.status == "success") {
-      popAdminData(adminData);
-      initAdminDeletion();
-    }
-  } catch (err) {
-    if (err) throw err;
-  }
 }
 
 function initUserDeletion() {
@@ -164,6 +106,28 @@ function initUserDeletion() {
   });
 }
 
+async function getAdminData() {
+  try {
+    let adminData = await fetch('/get-all-admins', {
+      method: 'GET'
+    });
+    adminData = await adminData.text();
+    adminData = JSON.parse(adminData);
+    if (adminData.status == "success") {
+      popAdminData(adminData);
+      initAdminDeletion();
+    }
+  } catch (err) {
+    if (err) throw err;
+  }
+}
+
+function popAdminData(adminData) {
+  // ADMIN TABLE CREATED HERE
+  let adminCard = makeAdminCard(adminData);
+  document.getElementById("admin-list").innerHTML = adminCard;
+}
+
 function initAdminDeletion() {
   document.getElementById("delete-admin").addEventListener("click", () => {
     let admin = document.getElementById("admin-username").value;
@@ -196,133 +160,33 @@ function initAdminDeletion() {
   });
 }
 
-async function sendData(data, path, printMessage) {
-  try {
-    let response = await fetch(path, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data)
-    });
-    response = await response.text();
-    response = JSON.parse(response);
-    printMessage(response);
-  } catch (err) {
-    if (err) throw err;
-  }
-}
-
 function searchUser() {
   document.querySelector('#search-user').addEventListener("click", function (e) {
     e.preventDefault();
     let userSearchInput = { username: document.querySelector('.search-input').value }
-    sendSearchData(userSearchInput);
+    sendData(userSearchInput, '/search-user', popUserCard);
     document.querySelector('.search-input').value = "";
   });
 }
+
+function popUserCard(searchData) {
+  // USER CARD CREATED HERE FOR SEARCH RESULT
+  let userCard = makeUserCard(searchData);
+  document.getElementById("search-results").innerHTML = userCard;
+ } 
 
 function searchAdmin() {
   document.querySelector('#search-admin').addEventListener("click", function (e) {
     e.preventDefault();
     let adminSearchInput = { username: document.querySelector('.search-admin-input').value }
-    sendAdminSearchData(adminSearchInput);
+    sendData(adminSearchInput, '.search-admin', popAdminCard);
     document.querySelector('.search-admin-input').value = "";
   });
 }
 
-async function sendSearchData(searchData) {
-  try {
-    let response = await fetch('/search-user', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(searchData)
-    });
-    response = await response.text();
-    response = JSON.parse(response);
-    popUserCard(response);
-  } catch (err) {
-    if (err) throw err;
-  }
-}
-
-async function sendAdminSearchData(searchData) {
-  try {
-    let response = await fetch('/search-admin', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(searchData)
-    });
-    response = await response.text();
-    response = JSON.parse(response);
-    popAdminCard(response);
-  } catch (err) {
-    if (err) throw err;
-  }
-}
-
-function popUserCard(searchData) {
- // USER CARD CREATED HERE FOR SEARCH RESULT
- let userCard = "<div class='user-card-group'>";
- for (let i = 0; i < searchData.rows.length; i++) {
-   userCard += (`
-   <div class='user-card'>
-
-     <div class='user-card-header'>
-       <div class='user-card-avatar-background'>
-         <img class='user-card-avatar' src='./avatars/${searchData.rows[i].profilePic}' alt='Profile Picture'>
-       </div>
-     </div>
-     <div class='user-card-info'>
-       <h3 class='user-card-username'>${searchData.rows[i].username}</h3>
-       <span class='user-card-business-name'>${searchData.rows[i].cName} | </span>
-       <span class='user-card-first-name'>${searchData.rows[i].fName} </span>
-       <span class='user-card-last-name'>${searchData.rows[i].lName}</span>
-     </div>
-     <a class='close' id='delete-user-account' href='#'>
-     <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512" width="16" height="16">
-       <g>
-         <path fill="currentColor" d="M286.161,255.867L505.745,36.283c8.185-8.474,7.951-21.98-0.523-30.165c-8.267-7.985-21.375-7.985-29.642,0   L255.995,225.702L36.411,6.118c-8.475-8.185-21.98-7.95-30.165,0.524c-7.985,8.267-7.985,21.374,0,29.641L225.83,255.867   L6.246,475.451c-8.328,8.331-8.328,21.835,0,30.165l0,0c8.331,8.328,21.835,8.328,30.165,0l219.584-219.584l219.584,219.584   c8.331,8.328,21.835,8.328,30.165,0l0,0c8.328-8.331,8.328-21.835,0-30.165L286.161,255.867z"/>
-       </g>
-       <span class="tooltip-text">Delete user</span>
-   </a>
-   </div>
-   `);
- }
- userCard += "</div>";
- document.getElementById("search-results").innerHTML = userCard;
-
-}
-
 function popAdminCard(searchData) {
   // ADMIN CARD CREATED HERE FOR SEARCH RESULT
-  let adminCard = "<div class='admin-card-group'>";
-  for (let i = 0; i < searchData.rows.length; i++) {
-    adminCard += (`
-    <div class='admin-card'>
-      <div class='admin-card-header'>
-        <div class='admin-card-avatar-background'>
-          <img class='admin-card-avatar' src="./avatars/${searchData.rows[i].profilePic}" alt='Profile Picture'>
-        </div>
-      </div>
-      <div class='admin-card-info'>
-        <h3 class='admin-card-username'>${searchData.rows[i].username}</h3>
-      </div>
-      <a class='close' id='delete-admin-account' href='#'>
-        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512" width="16" height="16">
-          <g>
-            <path fill="currentColor" d="M286.161,255.867L505.745,36.283c8.185-8.474,7.951-21.98-0.523-30.165c-8.267-7.985-21.375-7.985-29.642,0   L255.995,225.702L36.411,6.118c-8.475-8.185-21.98-7.95-30.165,0.524c-7.985,8.267-7.985,21.374,0,29.641L225.83,255.867   L6.246,475.451c-8.328,8.331-8.328,21.835,0,30.165l0,0c8.331,8.328,21.835,8.328,30.165,0l219.584-219.584l219.584,219.584   c8.331,8.328,21.835,8.328,30.165,0l0,0c8.328-8.331,8.328-21.835,0-30.165L286.161,255.867z"/>
-          </g>
-        <span class="tooltip-text">Remove admin privilege</span>
-      </a>
-    </div>
-    `);
-  }
-  adminCard += "</div>";
+  let adminCard = makeAdminCard(searchData);
   document.getElementById("search-results-2").innerHTML = adminCard;
  }
 
@@ -377,4 +241,62 @@ function toggleAdminSearchButton () {
     clearSearch.style.display = "none";
     searchButton.innerHTML = "Search"
   }
+}
+
+function makeUserCard(userData) {
+  let userCard = "<div class='user-card-group'>";
+ for (let i = 0; i < userData.rows.length; i++) {
+   userCard += (`
+   <div class='user-card'>
+
+     <div class='user-card-header'>
+       <div class='user-card-avatar-background'>
+         <img class='user-card-avatar' src='./avatars/${userData.rows[i].profilePic}' alt='Profile Picture'>
+       </div>
+     </div>
+     <div class='user-card-info'>
+       <h3 class='user-card-username'>${userData.rows[i].username}</h3>
+       <span class='user-card-business-name'>${userData.rows[i].cName} | </span>
+       <span class='user-card-first-name'>${userData.rows[i].fName} </span>
+       <span class='user-card-last-name'>${userData.rows[i].lName}</span>
+     </div>
+     <a class='close' id='delete-user-account' href='#'>
+     <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512" width="16" height="16">
+       <g>
+         <path fill="currentColor" d="M286.161,255.867L505.745,36.283c8.185-8.474,7.951-21.98-0.523-30.165c-8.267-7.985-21.375-7.985-29.642,0   L255.995,225.702L36.411,6.118c-8.475-8.185-21.98-7.95-30.165,0.524c-7.985,8.267-7.985,21.374,0,29.641L225.83,255.867   L6.246,475.451c-8.328,8.331-8.328,21.835,0,30.165l0,0c8.331,8.328,21.835,8.328,30.165,0l219.584-219.584l219.584,219.584   c8.331,8.328,21.835,8.328,30.165,0l0,0c8.328-8.331,8.328-21.835,0-30.165L286.161,255.867z"/>
+       </g>
+       <span class="tooltip-text">Delete user</span>
+   </a>
+   </div>
+   `);
+ }
+ userCard += "</div>";
+ return userCard;
+}
+
+function makeAdminCard(adminData) {
+  let adminCard = "<div class='admin-card-group'>";
+  for (let i = 0; i < adminData.rows.length; i++) {
+    adminCard += (`
+    <div class='admin-card'>
+      <div class='admin-card-header'>
+        <div class='admin-card-avatar-background'>
+          <img class='admin-card-avatar' src="./avatars/${adminData.rows[i].profilePic}" alt='Profile Picture'>
+        </div>
+      </div>
+      <div class='admin-card-info'>
+        <h3 class='admin-card-username'>${adminData.rows[i].username}</h3>
+      </div>
+      <a class='close' id='delete-admin-account' href='#'>
+        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512" width="16" height="16">
+          <g>
+            <path fill="currentColor" d="M286.161,255.867L505.745,36.283c8.185-8.474,7.951-21.98-0.523-30.165c-8.267-7.985-21.375-7.985-29.642,0   L255.995,225.702L36.411,6.118c-8.475-8.185-21.98-7.95-30.165,0.524c-7.985,8.267-7.985,21.374,0,29.641L225.83,255.867   L6.246,475.451c-8.328,8.331-8.328,21.835,0,30.165l0,0c8.331,8.328,21.835,8.328,30.165,0l219.584-219.584l219.584,219.584   c8.331,8.328,21.835,8.328,30.165,0l0,0c8.328-8.331,8.328-21.835,0-30.165L286.161,255.867z"/>
+          </g>
+        <span class="tooltip-text">Remove admin privilege</span>
+      </a>
+    </div>
+    `);
+  }
+  adminCard += "</div>";
+  return adminCard;
 }

df3feee49d6700c228308286d1e0cf4b8fcf731f
Betty Nguyen <bettyanhn@gmail.com>
2022-05-20 02:53:35 -0700

    Change back to localhost

--- scripts/chat.js
+++ scripts/chat.js
@@ -2,22 +2,22 @@
 let socket = io.connect('/');
 
 let messageContainer = document.querySelector('.message-container');
-let messageForm = document.querySelector('.message-block');
+let messageSend = document.querySelector('.message-send-block');
 let messageInput = document.querySelector('#message-input');
 
 socket.on('chat-message', data => {
   console.log(data);
-  appendMessage(data);
+  addMessage(data);
 });
 
-messageForm.addEventListener('click', e => {
+messageSend.addEventListener('click', e => {
   e.preventDefault();
   let message = messageInput.value;
   socket.emit('send-message', message);
   messageInput.value = "";
 });
 
-function appendMessage(message) {
+function addMessage(message) {
   let messageElement = document.createElement('div');
   messageElement.innerHTML += message;
   messageContainer.append(messageElement);

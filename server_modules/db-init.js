78785a0870f0d84e2c84d13e9c7b5c71877048ec
Betty Nguyen <bettyanhn@gmail.com>
2022-05-26 13:56:31 -0700

    Fix layout issue on profile page

--- server_modules/db-init.js
+++ server_modules/db-init.js
@@ -22,22 +22,11 @@ module.exports = {
     }
 }
 
-// async function initLocalDB() {
-//     const con = await mysql.createConnection({
-//         host: '127.0.0.1',
-//         user: 'root',
-//         password: ' ',
-//         multipleStatements: true
-//     });
-//     await con.query(`CREATE DATABASE IF NOT EXISTS COMP2800; USE COMP2800;`);
-//     con.destroy();
-// }
-
 async function initLocalDB() {
     const con = await mysql.createConnection({
-        host: 'localhost',
+        host: '127.0.0.1',
         user: 'root',
-        password: '',
+        password: ' ',
         multipleStatements: true
     });
     await con.query(`CREATE DATABASE IF NOT EXISTS COMP2800; USE COMP2800;`);

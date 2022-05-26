2ffdf917135faa14809252ad2d4fb4771a27d65b
Caroline Lin <caroline.yclin@gmail.com>
2022-05-25 17:34:28 -0700

    added windows version of localhosting credentials

--- server_modules/db-init.js
+++ server_modules/db-init.js
@@ -22,11 +22,22 @@ module.exports = {
     }
 }
 
+// async function initLocalDB() {
+//     const con = await mysql.createConnection({
+//         host: '127.0.0.1',
+//         user: 'root',
+//         password: ' ',
+//         multipleStatements: true
+//     });
+//     await con.query(`CREATE DATABASE IF NOT EXISTS COMP2800; USE COMP2800;`);
+//     con.destroy();
+// }
+
 async function initLocalDB() {
     const con = await mysql.createConnection({
-        host: '127.0.0.1',
+        host: 'localhost',
         user: 'root',
-        password: ' ',
+        password: '',
         multipleStatements: true
     });
     await con.query(`CREATE DATABASE IF NOT EXISTS COMP2800; USE COMP2800;`);
